4cea2322bdc9688930f8ffcb12d1a843c6477c35
707bane <707bane@gmail.com>
2025-10-24 10:10:53 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Tailles des principaux types de base :\n\n");
+    printf("Tailles des types de base (en octets) :\n\n");
 
-    printf("char : %d octet(s)\n", (int)sizeof(char));
-    printf("short : %d octet(s)\n", (int)sizeof(short));
-    printf("int : %d octet(s)\n", (int)sizeof(int));
-    printf("long : %d octet(s)\n", (int)sizeof(long));
-    printf("long long : %d octet(s)\n", (int)sizeof(long long));
-    printf("float : %d octet(s)\n", (int)sizeof(float));
-    printf("double : %d octet(s)\n", (int)sizeof(double));
-    printf("long double : %d octet(s)\n", (int)sizeof(long double));
+    printf("signed char : %d\n", (int)sizeof(signed char));
+    printf("unsigned char : %d\n\n", (int)sizeof(unsigned char));
+
+    printf("short : %d\n", (int)sizeof(short));
+    printf("unsigned short : %d\n\n", (int)sizeof(unsigned short));
+
+    printf("int : %d\n", (int)sizeof(int));
+    printf("unsigned int : %d\n\n", (int)sizeof(unsigned int));
+
+    printf("long int : %d\n", (int)sizeof(long int));
+    printf("unsigned long int : %d\n\n", (int)sizeof(unsigned long int));
+
+    printf("long long int : %d\n", (int)sizeof(long long int));
+    printf("unsigned long long int : %d\n\n", (int)sizeof(unsigned long long int));
+
+    printf("float : %d\n", (int)sizeof(float));
+    printf("double : %d\n", (int)sizeof(double));
+    printf("long double : %d\n", (int)sizeof(long double));
 
     return 0;
 }
