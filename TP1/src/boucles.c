5ce64883722c1758d7b59dbea66167150e6e02c4
707bane <707bane@gmail.com>
2025-10-24 10:26:34 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    int compteur = 5; 
+int main(void) {
+    int compteur = 5; // < 10
 
-    printf("Affichage d'un triangle rectangle avec des boucles (compteur = %d)\n\n", compteur);
-
+    if (compteur <= 0 || compteur >= 10) return 0;
 
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            printf("* ");
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
         }
         printf("\n");
     }
-
     return 0;
 }
