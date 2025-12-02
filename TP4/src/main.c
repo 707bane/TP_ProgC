7e4531e4fb94ce690f0e7f5bd888918d92462dff
707bane <707bane@gmail.com>
2025-12-02 10:55:35 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "operator.h"
 
+int main() {
+
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    int result = operateur(num1, num2, op);
+
+    printf("Resultat : %d\n", result);
+
+    return 0;
+}
