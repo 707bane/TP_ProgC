da351795e5a7a09ecaaf6a3c9a4450ee63204e1b
707bane <707bane@gmail.com>
2025-12-02 10:54:17 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,9 @@
+int somme(int a, int b);
+int difference(int a, int b);
+int produit(int a, int b);
+int quotient(int a, int b);
+int modulo(int a, int b);
+int et(int a, int b);
+int ou(int a, int b);
+int negation(int a);
+int operateur(int a, int b, char op);
