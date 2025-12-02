40a134e7d01e9cadb573a01e418424216f6c561b
707bane <707bane@gmail.com>
2025-12-02 09:47:08 +0100

    Update tri.c

---
+++ TP3/src/tri.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    int t[100];
+    srand(time(NULL));
+
+    for (int i = 0; i < 100; i++)
+        t[i] = rand() % 200 - 100;
+
+    printf("Tableau non trie :\n");
+    for (int i = 0; i < 100; i++)
+        printf("%d ", t[i]);
+    printf("\n");
+
+    for (int i = 0; i < 99; i++)
+        for (int j = 0; j < 99 - i; j++)
+            if (t[j] > t[j + 1]) {
+                int tmp = t[j];
+                t[j] = t[j + 1];
+                t[j + 1] = tmp;
+            }
+
+    printf("Tableau trie par ordre croissant :\n");
+    for (int i = 0; i < 100; i++)
+        printf("%d ", t[i]);
+    printf("\n");
+
+    return 0;
+}
